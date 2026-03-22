98786bf051e3dff37c17f553b5569aba6b645c2d
sir12321 <cs1240351@iitd.ac.in>
2026-03-22 11:47:39 +0530

    Revert "migrated sql and fixed seed bugs"
    This reverts commit 67d964ac0c8a1563973d1037e73d34acee39b2d8.

--- backend/src/utils/prisma.ts
+++ backend/src/utils/prisma.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
-const adapter = new PrismaBetterSqlite3({ url: 'file:./prisma/dev.db' });
+const adapter = new PrismaBetterSqlite3({ url: 'file:./dev.db' });
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
